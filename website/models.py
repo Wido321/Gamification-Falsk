ee78909d57b72ce1078e98417350550d7e9c9e39
Wido321 <109520982+Wido321@users.noreply.github.com>
2025-10-12 16:19:42 +0200

    Update models.py

--- website/models.py
+++ website/models.py
@@ -9,6 +9,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    color_facile_correct = db.Column(db.String(7), default='#a7f3bf')
+    color_facile_wrong = db.Column(db.String(7), default='#ffb6c1')
+    color_normale_correct = db.Column(db.String(7), default='#bfff00')
+    color_normale_wrong = db.Column(db.String(7), default='#e53935')
+    color_difficile_correct = db.Column(db.String(7), default='#43ea7f')
+    color_difficile_wrong = db.Column(db.String(7), default='#800020')
 
 
 class Tabella(db.Model):
@@ -19,3 +25,12 @@ class Tabella(db.Model):
     probabilita = db.Column(db.Float, default=0.0, nullable=False)
     stato = db.Column(db.String(20), default='-'*20, nullable=False)  # 20 chars: -- empty, Gf green facile, etc.
 
+
+
+class Storico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    cognome = db.Column(db.String(100), nullable=False)
+    difficolta = db.Column(db.String(20), nullable=False)
+    risultato = db.Column(db.String(10), nullable=False)
+    data = db.Column(db.DateTime, default=func.now())
